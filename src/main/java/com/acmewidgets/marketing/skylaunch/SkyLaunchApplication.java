de1aa6071e0529b14a8d1816582d2539d6e5b55d
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-07-10 16:22:47 +0200

    Initial Commit

---
+++ src/main/java/com/acmewidgets/marketing/skylaunch/SkyLaunchApplication.java
@@ -0,0 +1,12 @@
+package com.acmewidgets.marketing.skylaunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyLaunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyLaunchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/acmewidgets/marketing/skylaunch/SkyLaunchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acmewidgets.marketing.skylaunch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkyLaunchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
